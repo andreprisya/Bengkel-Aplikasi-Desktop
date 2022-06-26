56cb5c0c572b02593001a21f4313b6fa0280733b
andreprisya <andre.p20ti@mahasiswa.pcr.ac.id>
2022-06-26 23:48:37 +0700

    Add : Pertemuan 8

---
+++ Pertemuan 8/BelajarControl/BelajarControl/ItemsControl3.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace BelajarControl
+{
+    /// <summary>
+    /// Interaction logic for ItemsControl3.xaml
+    /// </summary>
+    public partial class ItemsControl3 : Window
+    {
+        public ItemsControl3()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Pertemuan 8/BelajarControl/BelajarControl/ListControl3.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace BelajarControl
+{
+    /// <summary>
+    /// Interaction logic for ListControl3.xaml
+    /// </summary>
+    public partial class ListControl3 : Window
+    {
+        public ListControl3()
+        {
+            InitializeComponent();
+            List<TodoItem> items = new List<TodoItem>();
+            items.Add(new TodoItem() { Judul = "Belajar Bengkel Aplikasi Desktop", Progres = 55 });
+            items.Add(new TodoItem() { Judul = "Belajar WPF dengan C#", Progres = 70 });
+            items.Add(new TodoItem() { Judul = "Membuat Laporan Praktikum", Progres = 0 });
+            lbTodoList.ItemsSource = items;
+        }
+
+        public class TodoItem
+        {
+            public string Judul { get; set; }
+            public int Progres { get; set; }
+        }
+    }
+}
